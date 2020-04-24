907b88c036bbf53c1243a3b449b5b387858145bc
Mitul Golakiya <mtl.golakiya@gmail.com>
2020-04-24 20:25:56 +0530

    fix: minor html structure change

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -5,14 +5,10 @@
         <h5 class="card-header">Tickets</h5>
         <div class="card-body">
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-lg-4">
                     <div class="input-group">
                         <input type="text" class="form-control" id="txtSearch" placeholder="Search">
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4">
                     <div class="tickets-list">
                         @foreach ($tickets as $ticket)
                             <div class="ticket">
@@ -26,11 +22,11 @@
                     <h3>Comments</h3>
                     <div class="comments-list">
                         @foreach ($comments as $comment)
-                        <div class="comment">
-                            <p class="card-subtitle mb-2 text-muted">{{ $comment->created_at->diffForHumans() }}</p>
-                            <p class="card-text">Quick sample text to create the card title and make up the body of the
-                                card's content.</p>
-                        </div>
+                            <div class="comment">
+                                <p class="card-subtitle mb-2 text-muted">{{ $comment->created_at->diffForHumans() }}</p>
+                                <p class="card-text">Quick sample text to create the card title and make up the body of the
+                                    card's content.</p>
+                            </div>
                         @endforeach
 
                     </div>
